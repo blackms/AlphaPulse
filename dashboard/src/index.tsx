ded608d3d6c70be2824d9e11254a60861c148be3
Alessio Rocchi <rocchi.b.a@gmail.com>
2025-03-07 23:33:45 +0100

    Remove application initialization at startup to prevent API spam

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -4,10 +4,15 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import App from './App';
 import './assets/styles/global.css';
+// Import but don't call initializeApp - it will be called after login
+import './initApp';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// We no longer initialize the application here
+// This will be done after login in the LoginPage component
 
 root.render(
   <React.StrictMode>
